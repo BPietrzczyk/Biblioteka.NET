067d2e129fa304659c31409a7cd295ef96b74659
James17PL <bpietrzczyk@gmail.com>
2021-06-07 20:43:19 +0200

    Dodano walidacje przy edytowaniu i usuwaniu ksiazki przez administratora. Dodano dodatkowe wyswietlanie informacji o ksiazkach

--- Pages/BookList/Index.cshtml.cs
+++ Pages/BookList/Index.cshtml.cs
@@ -20,25 +20,58 @@ namespace Projekt_Biblioteka.Pages.BookList
             _db = db;
         }
 
+        public string Error = "";
         public IEnumerable<Book> Books { get; set; }
+        public IEnumerable<BorrowedList> BorrowedBooks { get; set; }
 
         public async Task OnGetAsync()
         {
 
                 Books = await _db.Book.ToListAsync();
+                BorrowedBooks = await _db.BorrowedLists.ToListAsync();
         }
 
         public async Task<IActionResult> OnPostDelete(int id)
         {
             var book = await _db.Book.FindAsync(id);
+            Books = await _db.Book.ToListAsync();
+            BorrowedBooks = await _db.BorrowedLists.ToListAsync();
             if (book == null)
             {
                 return NotFound();
+            }
+            foreach(BorrowedList borrowed in BorrowedBooks)
+            {
+                if (borrowed.BookId.Equals(book.Id))
+                {
+                    Error = "Nie mo¿na usun¹æ wypo¿yczonej ksi¹¿ki!";
+                    return Page();
+                }
             }
             _db.Book.Remove(book);
             await _db.SaveChangesAsync();
 
             return RedirectToPage("Index");
+        }
+
+        public async Task<IActionResult> OnPostEdit(int id)
+        {
+            var book = await _db.Book.FindAsync(id);
+            Books = await _db.Book.ToListAsync();
+            BorrowedBooks = await _db.BorrowedLists.ToListAsync();
+            if (book == null)
+            {
+                return NotFound();
+            }
+            foreach (BorrowedList borrowed in BorrowedBooks)
+            {
+                if (borrowed.BookId.Equals(book.Id))
+                {
+                    Error = "Nie mo¿na edytowaæ wypo¿yczonej ksi¹¿ki!";
+                    return Page();
+                }
+            }
+            return RedirectToPage("Edit", new { id = id });
         }
     }
 }
